122cbc88f189b63aa7f3b20c2af17b7d59fa9bfa
roshansaini18 <135322753+roshansaini18@users.noreply.github.com>
2024-05-25 18:02:50 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,48 +1,11 @@
-function moveNavItems() {
-  const navbar = document.getElementById('navbar');
-  const dropdown = document.getElementById('dropdown');
-  const dropdownContent = document.getElementById('dropdown-content');
-  const navItems = Array.from(document.querySelectorAll('.nav-item'));
-  
-  // Reset navbar and dropdownContent
-  while (dropdownContent.firstChild) {
-    navbar.insertBefore(dropdownContent.firstChild, dropdown);
-  }
-  
-  // Ensure dropdown is hidden initially
-  dropdown.style.display = 'none';
-  
-  // Calculate available width
-  const navbarWidth = navbar.clientWidth;
-  let totalWidth = dropdown.clientWidth; // Include the dropdown itself in the width calculation
 
-  // Move items to dropdown if they overflow the navbar
-  for (let i = 0; i < navItems.length; i++) {
-    const item = navItems[i];
-    totalWidth += item.clientWidth;
-    if (1.5*totalWidth > navbarWidth) {
-      dropdownContent.appendChild(item);
-    }
-  }
-
-  // Show dropdown if it has any items
-  if (dropdownContent.children.length > 0) {
-    dropdown.style.display = 'block';
-  }
-}
-
-// Call the function initially to set the correct position
-window.addEventListener('load', moveNavItems);
-
-// Add event listener to call the function on window resize
-window.addEventListener('resize', moveNavItems);
 
 window.addEventListener('scroll', () => {
-  const nav = document.getElementById('nav');
+  const logo = document.getElementById('logo');
   if (window.scrollY > 50) {
-      nav.classList.add('scrolled');
+      logo.classList.add('scrolled');
   } else {
-      nav.classList.remove('scrolled');
+      logo.classList.remove('scrolled');
   }
 });
 window.addEventListener('scroll', () => {
@@ -69,4 +32,5 @@ if (window.scrollY > 50) {
             xhr.send();
         };
       
+       
        
